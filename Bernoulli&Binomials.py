25bfa94da4d8e810970ee3be8a1e2f6c53e8d841
seyedkazemi <50775784+seyedkazemi@users.noreply.github.com>
2019-05-27 14:20:23 +0430

    Update and rename Zar-Bernoulli&Binomials.py to Bernoulli&Binomials.py

--- Bernoulli&Binomials.py
+++ Bernoulli&Binomials.py
@@ -1,12 +1,12 @@
 from math import factorial
 
 
-print("\n      --- --- --- Zar Örnegi --- --- ---")
+print("\n      --- --- --- Para Örnegi --- --- ---")
 print("             EHSAN SEYEDKAZEMI AR  \n\n")
 
-print("X Zar atılmış")
+print("X Para atılmış")
 x= int(input("Please Enter X : "))
-print("\nY zar Yazı Gelmiş")
+print("\nY Para Yazı Gelmiş")
 y=int(input("Please Enter Y : "))
 if x<y :
     print("ERORR..  x<y : it's not possible")
@@ -15,6 +15,6 @@ if x<y :
 c = factorial(x)/(factorial(x-y)*factorial(y))
 R = c*(0.5**y)*(0.5**(x-y))
 print("\n\n")
-print(x," Zar atılmış ve ",y," zar Yazı Gelmiş Olasiliki : ",R)
+print(x," Para atılmış ve ",y," Para Yazı Gelmiş Olasiliki : ",R)
 
 print("\n\n        ------------ END ---------- ")
